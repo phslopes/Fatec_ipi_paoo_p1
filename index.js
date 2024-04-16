dd001c82467f926df65d4c59e8952637dd7b56c9
Pedro Lopes <pedroppzs01@gmail.com>
2024-04-16 19:17:25 -0300

    feat(p1);obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ APPID,
 Q,
 LIMIT,
 URLBASE,
+URLBASE2,
+UNIT,
 }=process.env // puxa as informações do .env
 
 const temp=()=>{
@@ -15,6 +17,13 @@ const temp=()=>{
         const latitude = res.data[0].lat//passa o caminho da resposta que quero buscar 
         const longitude = res.data[0].lon
         console.log(`${nome}:latitude:${latitude},longitude:${longitude}`) 
+    const url2 = `${URLBASE2}?lat=${latitude}&lon=${longitude}&unit=${UNIT}&appid=${APPID}`
+    axios.get(url2)
+    .then(res=>{
+        const feels_like = res.data.main.feels_like//passa o caminho da resposta que quero buscar 
+        const description = res.data.weather[0].description
+        console.log(`sensação termica:${feels_like},\ndescription:${description}`) 
+    })
     })
 }
 temp()
